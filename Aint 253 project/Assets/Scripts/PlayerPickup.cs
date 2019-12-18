fa442d0c485dc33ae7c4e91673f62aeb60e80285
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2019-12-18 16:58:36 +0000

    Added sound effects to buttons and picking up shapes

--- Aint 253 project/Assets/Scripts/PlayerPickup.cs
+++ Aint 253 project/Assets/Scripts/PlayerPickup.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class PlayerPickup : MonoBehaviour
 {
     public Transform theDest;
+    public AudioClip pickedUp;
+    public AudioSource audio;
 
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
     public void OnMouseDown()
     {
         GetComponent<Rigidbody>().useGravity = false;
         this.transform.position = theDest.position;
         this.transform.parent = GameObject.Find("held item").transform;
+        audio.PlayOneShot(pickedUp, 0.6f);
+
     }
 
     public void OnMouseUp()
